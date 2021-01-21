8312dca4010a71603b0ff7ed1b918120ff16c8ca
lucaott <luottmkl@gmail.com>
2021-01-21 11:26:33 +0100

    Added Navigator

---
+++ src/main/java/de/lxca1909/lobbysystem/items/Navigator.java
@@ -0,0 +1,19 @@
+package de.lxca1909.lobbysystem.items;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Navigator implements Listener {
+
+    @EventHandler
+    public void on(PlayerInteractEvent e){
+        Player p = e.getPlayer();
+        if(e.getItem() == null)return;
+        if(e.getItem().getType() == Material.COMPASS){
+            p.teleport(p.getLocation());
+        }
+    }
+}
